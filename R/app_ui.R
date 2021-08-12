82c84b86ca2eae4774d4de30ee8236febe340304
iofeidis <jasonof75@gmail.com>
2021-08-12 14:29:35 +0300

    Update vignette
    > Change order of sections
    
    > Add new logo (both vignette and app)

--- R/app_ui.R
+++ R/app_ui.R
@@ -1889,7 +1889,7 @@ golem_add_external_resources <- function() {
     )
 
     tags$head(
-        favicon(ext = "png"),
+        favicon(ext = "ico"),
         bundle_resources(
             path = app_sys("app/www"),
             app_title = "tripr"
